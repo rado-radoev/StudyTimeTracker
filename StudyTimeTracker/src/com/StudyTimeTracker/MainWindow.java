905ef284396e9e9c1571b735bf6198dc0aa044c4
Radoslav Radoev <rradoev@gmail.com>
2017-07-24 15:14:56 -0700

    initial coding

---
+++ StudyTimeTracker/src/com/StudyTimeTracker/MainWindow.java
@@ -0,0 +1,62 @@
+package com.StudyTimeTracker;
+
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
+
+public class MainWindow extends JFrame {
+	
+	private final JLabel currentActivityLabel;
+//	private final JLabel currentDateTimeLabel;
+//	private final JLabel timeStartedStudyingLabel;
+//	private final JLabel timeStoppedStudyingLabel;
+//	private final JLabel totalStudyTimeLabel;
+//	private final JLabel totalStudyTimeTodayLabel;
+	private final BorderLayout mainLayout;
+	private final JPanel currentActivitiPanel;
+	private final ButtonGroup currentActivityGroup;
+	private final JRadioButton studyRadioButton;
+	private final JRadioButton exercicesRadioButton;
+	private final JRadioButton projectsRadioButton;
+	
+
+	public MainWindow() {
+		super();
+		mainLayout = new BorderLayout();
+		
+		currentActivitiPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		currentActivityLabel = new JLabel("Current Activity");
+		currentActivitiPanel.add(currentActivityLabel);
+		
+		
+		
+		
+		currentActivityGroup = new ButtonGroup();
+		studyRadioButton = new JRadioButton("Study", true);
+		currentActivityGroup.add(studyRadioButton);
+		exercicesRadioButton = new JRadioButton("Exercices", false);
+		currentActivityGroup.add(exercicesRadioButton);
+		projectsRadioButton = new JRadioButton("Projects", false);
+		currentActivityGroup.add(projectsRadioButton);
+		
+		currentActivitiPanel.add(studyRadioButton);
+		currentActivitiPanel.add(exercicesRadioButton);
+		currentActivitiPanel.add(projectsRadioButton);
+		
+		
+		add(currentActivitiPanel);
+	}
+
+	
+}
---
+++ StudyTimeTracker/src/com/StudyTimeTracker/MainWindowTest.java
@@ -0,0 +1,16 @@
+package com.StudyTimeTracker;
+
+import javax.swing.JFrame;
+
+public class MainWindowTest {
+
+	public static void main(String[] args) {
+		MainWindow mw = new MainWindow();
+		mw.setTitle("Study Time Tracking");
+		mw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mw.setVisible(true);
+		mw.setSize(650, 450);// TODO Auto-generated method stub
+
+	}
+
+}
