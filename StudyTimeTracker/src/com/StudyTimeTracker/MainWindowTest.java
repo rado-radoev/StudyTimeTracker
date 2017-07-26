8626c1637afdf4e348775f221d6510004363be5d
Radoslav Radoev <rradoev@gmail.com>
2017-07-26 16:16:19 -0700

    added iteration 1 screenshot

--- StudyTimeTracker/src/com/StudyTimeTracker/MainWindowTest.java
+++ StudyTimeTracker/src/com/StudyTimeTracker/MainWindowTest.java
@@ -14,7 +14,7 @@ public class MainWindowTest {
 		mw.setTitle("Study Time Tracking");
 		mw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mw.setVisible(true);
-		mw.setSize(650, 450);
+		mw.setSize(590, 450);
 		mw.setResizable(false);
 		mw.setLocationRelativeTo(null);
 	}
