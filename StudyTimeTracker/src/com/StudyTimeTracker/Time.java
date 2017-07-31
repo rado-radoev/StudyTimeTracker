3acfb433cb3872b85198bb1d2d32ee407255d9d7
Radoslav Radoev <rradoev@gmail.com>
2017-07-31 13:26:28 -0700

    The time class will be the one that records the time in different files. Study, exercice or projects depending on the readio button selected. For now let's make it work for Study and we'll take care of the rest later.

--- StudyTimeTracker/src/com/StudyTimeTracker/Time.java
+++ StudyTimeTracker/src/com/StudyTimeTracker/Time.java
@@ -1,8 +1,67 @@
 package com.StudyTimeTracker;
 
-import com.StudyTimeTracker.Study;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Time {
 	
+	private LocalTime timeStartedStudying;
+	private LocalTime timeStoppedStudying;
+	private LocalTime totalStudyTimeToday;
+	private LocalTime totalStudyTime;
+	private LocalDate today;
 	
+	public Time() {
+		
+	}
+	
+	public Time(LocalTime timeStartedStudying, LocalTime timeStoppedStudying, LocalTime totalStudyTimeToday,
+			LocalTime totalStudyTime, LocalDate today) {
+		
+		setTimeStartedStudying(timeStartedStudying);
+		setTimeStoppedStudying(timeStoppedStudying);
+		setTotalStudyTimeToday(totalStudyTimeToday);
+		setTotalStudyTime(totalStudyTime);
+		setToday(today);		
+	}
+
+	public LocalTime getTimeStartedStudying() {
+		return timeStartedStudying;
+	}
+
+	public void setTimeStartedStudying(LocalTime timeStartedStudying) {
+		this.timeStartedStudying = timeStartedStudying;
+	}
+
+	public LocalTime getTimeStoppedStudying() {
+		return timeStoppedStudying;
+	}
+
+	public void setTimeStoppedStudying(LocalTime timeStoppedStudying) {
+		this.timeStoppedStudying = timeStoppedStudying;
+	}
+
+	public LocalTime getTotalStudyTimeToday() {
+		return totalStudyTimeToday;
+	}
+
+	public void setTotalStudyTimeToday(LocalTime totalStudyTimeToday) {
+		this.totalStudyTimeToday = totalStudyTimeToday;
+	}
+
+	public LocalTime getTotalStudyTime() {
+		return totalStudyTime;
+	}
+
+	public void setTotalStudyTime(LocalTime totalStudyTime) {
+		this.totalStudyTime = totalStudyTime;
+	}
+
+	public LocalDate getToday() {
+		return today;
+	}
+
+	public void setToday(LocalDate today) {
+		this.today = today;
+	}
 }
